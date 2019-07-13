0e35797998947058858a6ad7720d988a2d9ed3e5
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 03:41:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if array.length > 0
+    i = 0
+    return_array = []
+    while i < array.length
+      return_array << yield(array[i])
+      i += 1
+    end
+    return_array
+  end
+end
